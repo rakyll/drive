db7506296775eaf8d788c6bcabeb7055f84e39c7
Emmanuel Odeke <odeke@ualberta.ca>
2015-01-08 20:40:21 -0700

    fixed quoting for paths + better help + pushes
    + Fixed bug in which any path with a quote would create
      an incorrect remote query ie oxy' -> title = 'oxy''
    + Fixed bug where pushes were being rejected
      if change.Src == nil.
    + Fixed the help string messages.

--- list.go
+++ list.go
@@ -169,7 +169,7 @@ func (g *Commands) breadthFirst(parentId, parent,
 
 	// A depth of < 0 means traverse as deep as you can
 	if depth == 0 {
-        // At the end of the line, this was successful.
+		// At the end of the line, this was successful.
 		return true
 	}
 	if depth > 0 {
